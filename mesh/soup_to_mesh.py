aa72028d2ad774bdcd676cb5d9acf897442f499c
Hallison Oliveira da Paz <hallpaaz@gmail.com>
2016-04-14 16:02:22 -0300

    end of work at impa. should complete boundary traversal later

--- mesh/soup_to_mesh.py
+++ mesh/soup_to_mesh.py
@@ -6,6 +6,51 @@ from DataStructures import Vertex, Triangle
 
 data_folder = "data/"
 
+def read_OFF(off_file):
+    vertexBuffer = []
+    indexBuffer = []
+    with open(off_file, "r") as modelfile:
+        first = modelfile.readline().strip()
+        if first != "OFF":
+            raise(Exception("not a valid OFF file ({})".format(first)))
+
+        parameters = modelfile.readline().strip().split()
+        min_distance = BIG
+        minx = BIG
+        miny = BIG
+        minz = BIG
+        maxx = -BIG
+        maxy = -BIG
+        maxz = -BIG
+
+        if len(parameters) < 2:
+            raise(Exception("OFF file has invalid number of parameters"))
+
+        for i in range(int(parameters[0])):
+            coordinates = modelfile.readline().split()
+            X, Y, Z = float(coordinates[0]), float(coordinates[1]), float(coordinates[2])
+            vertexBuffer.append(Vertex(X, Y, Z))
+            if X < minx:
+                minx = X
+            if X > maxx:
+                maxx = X
+            if Y < miny:
+                miny = Y
+            if Y > maxy:
+                maxy = Y
+            if Z < minz:
+                minz = Z
+            if Z > maxz:
+                maxz = Z
+
+        for i in range(int(parameters[1])):
+            indices = modelfile.readline().split()
+            indexBuffer.append(Triangle(int(indices[1]), int(indices[2]), int(indices[3])))
+
+        bounding_box = BoundingBox(minx, miny, minz-1, maxx, maxy, maxz+1)
+
+    return vertexBuffer, indexBuffer, bounding_box
+
 def soup_to_mesh(filename: str, output_file: str, dimension = 2):
     vertices = []
     indices = []
@@ -38,6 +83,20 @@ def soup_to_mesh(filename: str, output_file: str, dimension = 2):
         '''%(len(vertices),len(indices), "".join(vertices), "".join(indices)))
 
 
+### Complete this ###
+def share_edge(edge: set, triangle: list):
+
+    if edge.intersection(triangle) == edge
+
+def face_association_from_OFF(filename: str):
+    vertices, indices = read_OFF(filename)
+
+
+    indices = [set([t.a, t.b, t.c]) for t in indices]
+    for t in indices:
+        [j for j in indices if ]
+        neighbors.append()
+#####################
 
 def main(input_file, dimension = 2):
     dot_index = input_file.rfind(".")
