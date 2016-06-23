410a4e14dcc96b4f4b39cf98ed4021556f484418
hallpaz <hallpaaz@gmail.com>
2016-06-23 14:52:28 -0300

    thinking about an efficient and elegant solution to split_long_edges method

--- remeshing.py
+++ remeshing.py
@@ -5,6 +5,7 @@ import os
 import matplotlib.pyplot as plt
 from math import pi, sqrt
 from geometrypack.dataio import read_OFF, write_PLY, write_OFF, write_uv_PLY
+from geometrypack.data_structures import MinPriorityQueue
 from geometrypack.algorithms import distance_vertex_plane, normalize
 from geometrypack import drawing
 from geometrypack.meshes import compute_neighborhood, compute_boundary_indices, rotate
@@ -13,8 +14,22 @@ from geometrypack.meshes import compute_neighborhood, compute_boundary_indices, 
 lowcoefficient = 4/5
 highcoefficient = 4/3
 
-def split_long_edges(high):
-    pass
+def split_long_edges(vertices, indices, high):
+    queue = MinPriorityQueue()
+    for triangle in indices:
+        for i in range(3):
+            edge = (triangle[i], triangle[(i+1)%3])
+            queue.add_point(edge_info, np.linalg.norm(edge[0] - edge[1]))
+    candidate = queue.pop_point()
+    while(candidate[0] > high):
+        edge = candidate[-1]
+        middle = (edge[0] + edge[1])/2
+        indices_triangles = [i_t for i_t in enumerate(indices) if set(edge).intersection(i_t[1]) == 2]
+        # indices[triangles[0][0]] = [edge[0], middle, set(triangles[1]).difference(edge)[0]]
+        # indices[triangles[1][0]] = [edge[0], middle, set(triangles[1]).difference(edge)[1]]
+        # indices.append([edge[0], middle, set(triangles[1]).difference(edge)[0]])
+        # indices.append([edge[0], middle, set(triangles[1]).difference(edge)[0]])
+
     # I will need a priority queue here
     # while exists edge e with length(e) > high:
     #     split e at midpoint
@@ -30,27 +45,27 @@ def collapse_short_edges(low, high):
     #     if should_collapse:
     #         collapse a into b along e
 
-def flip(edge):
+def flip(indices, edge):
     pass
 
-def valence(vindex):
-    pass
+# number of neighbors a vertex vindex has
+def valence(vindex, indices):
+    return len([t for t in indices if vindex in t])
 
-def equalize_valences():
-    pass
+def equalize_valences(indices, targetval=6):
     # for edge in edges:
     #     le a, b, c, d be the vertices of the two triangles adjacent to edge
     #     deviation_pre = (abs(valence(a) - targetval(a))
     #                     + abs(valence(b) - targetval(b))
     #                     + abs(valence(c) - targetval(c))
     #                     + abs(valence(d) - targetval(d)))
-    #     flip(edge)
+    #     flip(indices, edge)
     #     deviation_post = (abs(valence(a) - targetval(a))
     #                     + abs(valence(b) - targetval(b))
     #                     + abs(valence(c) - targetval(c))
     #                     + abs(valence(d) - targetval(d)))
     #     if deviation_pre <= deviation_post:
-    #         flip(e)
+    #         flip(indices, e)
 
 def tangential_relaxation(vindex, normal, neighbors):
     q = sum(neighbors)
@@ -61,12 +76,13 @@ def project_to_surface():
     pass
 
 #polygon mesh processing incremental remeshing
-def remesh(target_length, maxiterations=10):
+def remesh(vertices, indices, target_length, maxiterations=10):
+    neighborhood = compute_neighborhood(vertices, indices)
     low = lowcoefficient*target_length
     high = highcoefficient*target_length
     for i in range(maxiterations):
-        split_long_edges(high)
-        collapse_short_edges(low, high)
-        equalize_valences()
+        split_long_edges(vertices, indices, high)
+        collapse_short_edges(vertices, indices, low, high)
+        equalize_valences(indices)
         tangential_relaxation()
         project_to_surface()
